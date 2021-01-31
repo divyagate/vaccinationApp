db5c279750f9e3cbb85cf10048f5d06f050c3be0
Tarunreddyamanolla <Amanollatarunreddy@gmail.com>
2021-01-31 17:29:24 -0500

    home and doctor fragment

---
+++ app/src/main/java/com/example/vaccinationapp/fragments/DoctorFragment.java
@@ -0,0 +1,16 @@
+package com.example.vaccinationapp.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vaccinationapp.R;
+
+public class DoctorFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_doctor);
+    }
+}

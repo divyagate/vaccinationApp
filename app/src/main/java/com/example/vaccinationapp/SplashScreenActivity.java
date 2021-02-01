e4f0e78e69b9dbd9c5ee367d3802da1f14220474
ManojReddy <manojmanoj252@gmail.com>
2021-01-31 17:08:15 -0700

    user story va1 : splash screen

--- app/src/main/java/com/example/vaccinationapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/vaccinationapp/SplashScreenActivity.java
@@ -2,13 +2,45 @@ package com.example.vaccinationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.example.vaccinationapp.helpers.Constants;
+import com.example.vaccinationapp.helpers.PrefManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    PrefManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Flag's for full screen.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        manager = new PrefManager(this);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (manager.getData(Constants.EMAIL_ID) == null || manager.getData(Constants.EMAIL_ID).equals("")) {
+                    //Do something after 3 Seconds
+                    Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                    finishAffinity();
+                } else {
+                    //Do something after 3 Seconds
+                    Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finishAffinity();
+                }
+            }
+        }, 3 * 1000);
     }
 }

bf523b73f0024ee1051c150b3a71d035a592de66
Tarunreddyamanolla <Amanollatarunreddy@gmail.com>
2021-02-13 19:42:22 -0500

    user story va11 : forgetpassword 2 xml file design , 1 ForgetPasswordActivity

---
+++ app/src/main/java/com/example/vaccinationapp/RestorePassActivity.java
@@ -0,0 +1,14 @@
+package com.example.vaccinationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RestorePassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restore_pass);
+    }
+}

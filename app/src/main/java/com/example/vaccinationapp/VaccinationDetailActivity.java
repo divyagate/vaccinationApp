15a76336a3f6fa834afb4b9d263b967743d64976
krishnavamsi <krishnavamsipolepalli97pk@gmail.com>
2021-02-15 18:26:00 -0700

    VaccinationDetailActivity xml and java file

--- app/src/main/java/com/example/vaccinationapp/VaccinationDetailActivity.java
+++ app/src/main/java/com/example/vaccinationapp/VaccinationDetailActivity.java
@@ -1,14 +1,48 @@
 package com.example.vaccinationapp;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.vaccinationapp.models.VaccineListModel;
 
 public class VaccinationDetailActivity extends AppCompatActivity {
+
+    ImageView mImageBack;
+    TextView mTextVaccineName, mTextDetails, mTextSchedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vaccination_detail);
+
+        mInitViews();
+        mInitStatements();
+        getData();
+    }
+
+    private void getData() {
+        try {
+            Intent intent = getIntent();
+            VaccineListModel.Item model = (VaccineListModel.Item) intent.getSerializableExtra("model");
+            mTextVaccineName.setText(model.getName());
+            mTextDetails.setText(model.getDetails());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void mInitViews() {
+        mImageBack = findViewById(R.id.back);
+
+        mTextVaccineName = findViewById(R.id.vaccination_name);
+        mTextDetails = findViewById(R.id.vaccination_detail);
+        mTextSchedule = findViewById(R.id.tobetaken);
+    }
+
+    private void mInitStatements() {
+        mImageBack.setOnClickListener(v -> onBackPressed());
     }
 }

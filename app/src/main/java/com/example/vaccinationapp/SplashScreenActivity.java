eaa4bdddcdb28714e06c5340fc35b18d919c4de0
Navjot <kaurnavjot1302@gmail.com>
2021-02-15 22:31:32 -0500

    localization setup in splash screen

--- app/src/main/java/com/example/vaccinationapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/vaccinationapp/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.vaccinationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -10,11 +12,14 @@ import android.view.WindowManager;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.example.vaccinationapp.helpers.Constants;
+import com.example.vaccinationapp.helpers.LocaleHelper;
 import com.example.vaccinationapp.helpers.PrefManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     PrefManager manager;
+    Context context;
+    Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +30,31 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         manager = new PrefManager(this);
 
+        if (!manager.getData(Constants.SELECTED_LANG).equals("")) {
+            if (manager.getData(Constants.SELECTED_LANG).equals("en")) {
+                context = LocaleHelper.setLocale(SplashScreenActivity.this, "en");
+                resources = context.getResources();
+            } else if (manager.getData(Constants.SELECTED_LANG).equals("fr")) {
+                context = LocaleHelper.setLocale(SplashScreenActivity.this, "fr");
+                resources = context.getResources();
+            } else {
+                context = LocaleHelper.setLocale(SplashScreenActivity.this, "en");
+                resources = context.getResources();
+            }
+        }
+
         final Handler handler = new Handler(Looper.getMainLooper());
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (manager.getData(Constants.EMAIL_ID) == null || manager.getData(Constants.EMAIL_ID).equals("")) {
-                    //Do something after 3 Seconds
-                    Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
-                    startActivity(intent);
-                    finishAffinity();
-                } else {
-                    //Do something after 3 Seconds
-                    Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
-                    startActivity(intent);
-                    finishAffinity();
-                }
+        handler.postDelayed(() -> {
+            if (manager.getData(Constants.EMAIL_ID) == null || manager.getData(Constants.EMAIL_ID).equals("")) {
+                //Do something after 3 Seconds
+                Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finishAffinity();
+            } else {
+                //Do something after 3 Seconds
+                Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finishAffinity();
             }
         }, 3 * 1000);
     }
